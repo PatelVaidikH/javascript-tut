dc2ffa5761fd411805b82b4540a21bc0abda42b5
Vaidik Patel <115892125+PatelVaidikH@users.noreply.github.com>
2024-01-19 15:22:58 +0000

    datatypes

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -1,3 +1,7 @@
+//  Primitive
+
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
 const score = 100
 const scoreValue = 100.3
 
@@ -10,16 +14,24 @@ const anotherId = Symbol('123')
 
 console.log(id === anotherId);
 
-const bigNumber = 34432786375752385n
+// const bigNumber = 3456543576654356754n
 
-const heros = ["shaktiman","naagraj","doga"]
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
 let myObj = {
-    name:"Vaidik",
-    age:19
+    name: "hitesh",
+    age: 22,
 }
 
 const myFunction = function(){
-    console.log("Hello World");
+    console.log("Hello world");
 }
 
-console.log(typeof bigNumber);
+console.log(typeof anotherId);
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
